bb1306cfddb676201c2d4b05f836f35df624da22
kiki <kikidprasetyo29@gmail.com>
2023-01-01 20:09:50 +0700

    for deploy

--- components/navbarUser.tsx
+++ components/navbarUser.tsx
@@ -1,36 +1,36 @@
-// import { FaArrowLeft } from 'react-icons/fa'
-// import { FaHome } from 'react-icons/fa';
+import { FaArrowLeft } from 'react-icons/fa'
+import { FaHome } from 'react-icons/fa';
 // import { MdLibraryBooks } from 'react-icons/Md';
-// import { BsPlusLg } from 'react-icons/bs';
-// import { FaBell } from 'react-icons/fa';
-// import { GoPerson } from 'react-icons/go';
-// import styles from '../styles/menuutama.module.css'
-// import Link from 'next/link';
+import { BsPlusLg } from 'react-icons/bs';
+import { FaBell } from 'react-icons/fa';
+import { GoPerson } from 'react-icons/go';
+import styles from '../styles/menuutama.module.css'
+import Link from 'next/link';
 
-// import React from 'react';
+import React from 'react';
 
-// function navbarUser(){
-//     return(
-//         <nav className={styles.icon}>
-//             <Link href="menuutama" className={styles.iconbar}>
-//                 <p><FaHome/></p>
-//             </Link>
-//             <Link href="#" className={styles.iconbar}>
-//                 <p><MdLibraryBooks/></p>
-//             </Link>
-//             <div className={styles.plus}>
-//                 <Link href="informasikolamdanlele" className={styles.tombolplus}>
-//                     <p><BsPlusLg/></p>
-//                 </Link>
-//             </div>
-//             <Link href="notifikasi" className={styles.iconbar}>
-//                 <p><FaBell/></p>
-//             </Link>
-//             <Link href="UserProfile" className={styles.iconbar}>
-//                 <p><GoPerson/></p>
-//             </Link>
-//         </nav>
-//     )
-// }
+function navbarUser(){
+    return(
+        <nav className={styles.icon}>
+            <Link href="menuutama" className={styles.iconbar}>
+                <p><FaHome/></p>
+            </Link>
+            <Link href="#" className={styles.iconbar}>
+                {/* <p><MdLibraryBooks/></p> */}
+            </Link>
+            <div className={styles.plus}>
+                <Link href="informasikolamdanlele" className={styles.tombolplus}>
+                    <p><BsPlusLg/></p>
+                </Link>
+            </div>
+            <Link href="notifikasi" className={styles.iconbar}>
+                <p><FaBell/></p>
+            </Link>
+            <Link href="UserProfile" className={styles.iconbar}>
+                <p><GoPerson/></p>
+            </Link>
+        </nav>
+    )
+}
 
-// export default navbarUser();
+export default navbarUser();
